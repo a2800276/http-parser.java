313a43cbbd528a3d3ef6cc551aa055d1db53a847
Tim Becker <tim.becker@kuriositaet.de>
2011-05-15 16:33:17 +0200

    added test_message3

--- src/test/http_parser/lolevel/Responses.java
+++ src/test/http_parser/lolevel/Responses.java
@@ -6,11 +6,50 @@ import java.util.*;
 import static http_parser.lolevel.Util.*;
 import http_parser.*;
 
+import primitive.collection.ByteList;
+
 public class Responses {
 
   
-  
+  static void test_multiple3(Message r1, Message r2, Message r3) {
+    int message_count = 1;
+    if (!r1.upgrade) {
+      message_count++;
+      if (!r2.upgrade) {
+        message_count++;
+      }
+    }
+    boolean has_upgrade = (message_count < 3 || r3.upgrade);
+    
+    ByteList blist = new ByteList();
+             blist.addAll(r1.raw);
+             blist.addAll(r2.raw);
+             blist.addAll(r3.raw);
 
+    byte [] raw = blist.toArray();
+    ByteBuffer buf   = ByteBuffer.wrap(raw);
+   
+    Util.Settings settings = Util.settings(); 
+    HTTPParser parser = new HTTPParser(r1.type);
+              parser.execute(settings, buf);
+    if (has_upgrade && parser.upgrade) {
+      check(settings.numCalled == message_count); 
+      return;
+    }
+    
+    check(buf.position() == buf.limit() && buf.position() == raw.length);
+    
+    buf = Util.empty();    
+    parser.execute(settings, buf);
+    if (has_upgrade && parser.upgrade) {
+      check(settings.numCalled == message_count); 
+      return;
+    }
+
+    check(buf.position() == buf.limit() && buf.position() == 0);
+    check(settings.numCalled == message_count); 
+  }  
+  
 
   public static void test () {
     List<Message> all = TestLoaderNG.load("tests.dumped");
@@ -29,7 +68,7 @@ public class Responses {
       for (int j = 0; j!=responses.size(); ++j) {
         if (!responses.get(j).should_keep_alive) continue;
         for (int k = 0; k!= responses.size(); ++k) {
-//          test_multiple3(responses.get(i), responses.get(j), responses.get(k));
+          test_multiple3(responses.get(i), responses.get(j), responses.get(k));
         }
       }
     }
